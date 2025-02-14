da03f14b7bc8524673501793b9f48e9527fd5a3e
jawad uthman <uthmanjawaad@gmail.com>
2025-02-14 11:29:31 +0100

    Added new contents

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -9,7 +12,11 @@ function App() {
 
   return (
     <>
+    <Header />
+    <MainContent />
+    <Footer />
     <WelcomeMessage />
+    
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
