817b6ec40a6fe0fba5a8bdad25da0705285fcd45
jawad uthman <uthmanjawaad@gmail.com>
2025-02-22 10:56:27 +0100

    Add Counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,3 +1,4 @@
+import Counter  from './components/Counter'
 import UserProfile from './components/UserProfile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <>
+    <Counter />
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     <Header />
     <MainContent />
