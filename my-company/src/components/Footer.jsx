2b57afb0bfd47e44f5a2167094d193f18d4cb261
jawad uthman <uthmanjawaad@gmail.com>
2025-02-23 00:20:22 +0100

    Add my-company

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ background: "#333", color: "white", padding: "10px", textAlign: "center", marginTop: "20px" }}>
+        <p>© 2025 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
